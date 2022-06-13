1ef673256a82255b509db7b0d90884cb6b2b4d8e
lu cía santos ordóñez <analucia0189@gmail.com>
2022-06-12 23:27:29 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const port = 5000
+const cors = require('cors')
+const he = require('he')
+const raejs = require('@jodacame/raejs');
+
+
+app.use(cors())
+
+app.get('/rae-api/:palabra', (req, res) => {
+  async function callrae(params) {
+    
+    const query = params;
+    const response = await raejs.search(query);
+    if(!response.error){
+      results = response.results
+      results.forEach(result => {
+        result.source = he.decode(result.source)
+        result.definition.forEach((def,ind) =>{
+          result.definition[ind] = he.decode(def)
+        })
+      });
+      res.send(results)
+    }
+    else{
+      res.send({"code":"404"})
+    }
+  }
+
+  callrae(req.params.palabra)
+  
+
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+  
+})
